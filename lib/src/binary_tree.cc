6d56f8c30267fb1fe147c8f5e034de3cf1aecddc
Emilio Garcia-Fidalgo <emilio.garcia@uib.es>
2017-10-31 17:22:08 +0100

    Fixing error in licenses

--- lib/src/binary_tree.cc
+++ lib/src/binary_tree.cc
@@ -14,7 +14,7 @@
 * GNU General Public License for more details.
 *
 * You should have received a copy of the GNU General Public License
-* along with bimos. If not, see <http://www.gnu.org/licenses/>.
+* along with obindex2. If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include "obindex2/binary_tree.h"
